437c230a2deacb5ce5fe292ebc236d029293475b
Abdulahi Mohamud <abdulahimohamud22@gmail.com>
2022-02-24 13:09:49 -0800

    minor updates

--- first.js
+++ first.js
@@ -9,12 +9,16 @@ function userNameInput (){
     }
 }
 function showImage(){
+
     var userImgResponce = prompt("DO YOU WANT TO BE ENLIGHTENED WITH A PIC?");
     let spaceImg = "https://spaceholder.cc/200x200"
+
     if(userImgResponce.toLowerCase() == "yes"){
         document.write('<img src="' + spaceImg + '">');
+
     if (userImgResponce.toLowerCase() == "no"){
          let lastChance = prompt("LAST CHANCE");
+         
         if (lastChance.toLowerCase() == "yes"){
             document.write('<img src="' + spaceImg + '">');  
     }   
