673a87d52428621b51f8c87e509b0fd5f3467287
Abdulahi Mohamud <abdulahimohamud22@gmail.com>
2022-02-24 13:00:23 -0800

    Added fuction to js to help the readability and to clean the code

--- first.js
+++ first.js
@@ -1,15 +1,25 @@
-var userName = prompt("Enter thy Name !");
-var userImgResponce = prompt("DO YOU WANT TO BE ENLIGHTENED WITH A PIC?");
-let spaceImg = "https://spaceholder.cc/200x200"
 
-if (userName == "abdulahi"){
-    alert("Welcome master !");
-}else{
-    
-    document.write("Welcome, " +userName + " to space.");
+function userNameInput (){
+    var userName = prompt("Enter thy Name !");
+    if (userName == "abdulahi"){
+        alert("Welcome master !");
+    }else{
+        
+        document.write("Welcome, " +userName + " to space.");
+    }
 }
-if(userImgResponce == "yes"){
-    document.write('<img src="' + spaceImg + '">');
-}else{
-    alert("YOU WILL REGRET IT!!!")
+function showImage(){
+    var userImgResponce = prompt("DO YOU WANT TO BE ENLIGHTENED WITH A PIC?");
+    let spaceImg = "https://spaceholder.cc/200x200"
+    if(userImgResponce.toLowerCase() == "yes"){
+        document.write('<img src="' + spaceImg + '">');
+    if (userImgResponce.toLowerCase() == "no"){
+         let lastChance = prompt("LAST CHANCE");
+        if (lastChance.toLowerCase() == "yes"){
+            document.write('<img src="' + spaceImg + '">');  
+    }   
+    }else{
+        alert("YOU WILL REGRET IT!!!");
+    }
+    
 }
