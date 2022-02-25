ceaa4f09eb9488f35ad6b91d42da25830fa3b38d
Abdulahi Mohamud <abdulahimohamud22@gmail.com>
2022-02-24 16:47:27 -0800

    added loops and fix the function to hace an optimal user experience

--- first.js
+++ first.js
@@ -1,6 +1,6 @@
 
-function userNameInput (){
-    var userName = prompt("Enter thy Name !");
+function userNameInput(){
+    let userName = prompt("Enter thy Name !");
     if (userName == "abdulahi"){
         alert("Welcome master !");
     }else{
@@ -8,6 +8,7 @@ function userNameInput (){
         document.write("Welcome, " +userName + " to space.");
     }
 }
+
 function showImage(){
 
     var userImgResponce = prompt("DO YOU WANT TO BE ENLIGHTENED WITH A PIC?");
@@ -15,15 +16,55 @@ function showImage(){
 
     if(userImgResponce.toLowerCase() == "yes"){
         document.write('<img src="' + spaceImg + '">');
-
+    }
     if (userImgResponce.toLowerCase() == "no"){
          let lastChance = prompt("LAST CHANCE");
          
-        if (lastChance.toLowerCase() == "yes"){
-            document.write('<img src="' + spaceImg + '">');  
-    }   
-    }else{
+         if (lastChance.toLowerCase() == "yes"){
+             document.write('<img src="' + spaceImg + '">');  
+    }   else {
         alert("YOU WILL REGRET IT!!!");
+        } 
+        
     }
     
+}
+
+function displayRating() {
+
+    var output = "";
+    var userRating = prompt("ADD STARS TO THE SKY ");
+    
+    for (let i = 0; i < userRating; i++) {
+
+        output += "🌟"
+
+    }
+
+    document.write(output);
+}
+
+function guessingGame() {
+    let userGuess = prompt("Enter a number between 1 & 10");
+    let rightAnswer = 7;
+    let tries = 3;
+
+    for (i = 0; i < tries; i++) {
+        while (userGuess < 1 || userGuess > 10) {
+            userGuess = prompt("Please enter a number between 1 & 10");
+        }
+        if (userGuess == rightAnswer) {
+            alert = alert("WINNER WINNER CHICKEN DINNER (unless ur vegan)");
+            document.write("CHAMP");
+            break;
+        }   else if (userGuess < rightAnswer) {
+            userGuess = prompt();
+        }   else if (userGuess > rightAnswer) {
+            userGuess = prompt("WRONG THE NUMBER IS LOWER");
+        }
+        if (i == 2) {
+            alert("HA,HA THE NUMBER WAS " + rightAnswer);
+            document.write("LOSER");
+        }
+    }
 }
